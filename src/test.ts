dbf094d9ece135bf63539e0eddf48b8ba1480c96
LowEntropyBody <jszyyx@163.com>
2025-01-14 11:44:19 +0800

    Fix error

--- src/test.ts
+++ src/test.ts
@@ -2,7 +2,7 @@
 import { Coinbase, Wallet } from "@coinbase/coinbase-sdk";
 import * as fs from 'fs/promises';
 
-Coinbase.configure({ apiKeyName: "organizations/009bf85e-8bd3-4cce-8e95-56a83f841c04/apiKeys/152988d0-0294-4195-91d3-3fe5a7d93e75", privateKey: "-----BEGIN EC PRIVATE KEY-----\nMHcCAQEEIM550SqXJHPc1vLEEUg9gatKv1+2tkYh/YB+hX0p9JTMoAoGCCqGSM49\nAwEHoUQDQgAEN8ZYx98QL4I1wVk87YpuJC+JKDKRMa3dcrvsA7E+OgQdncnlLu5O\npVL2KJhznnZPwKPzKrVgPICQ9sAexUhFdw==\n-----END EC PRIVATE KEY-----\n" });
+Coinbase.configure({ apiKeyName: "", privateKey: "" });
 
 async function queryMpcWallet() {
     try {
